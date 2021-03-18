75ba8ecc75a13da10bfdb6bdd98d5a98cc321e57
Mark Elston <Mark.Elston@advantest.com>
2021-03-17 17:35:05 -0700

    Add project files.

---
+++ DeferToBGThread/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeferToBGThread
+{
+
+    class LLA
+    {
+        LLA() { }
+
+    }
+
+    class TThread
+    {
+        private int id;
+        private int saveTime;
+        private int getWaitTime;
+
+        TThread(int id, int saveTime, int getWaitTime)
+        {
+            this.id = id;
+            this.saveTime = saveTime;
+            this.getWaitTime = getWaitTime;
+        }
+
+        void run()
+        {
+            Thread.Sleep(saveTime);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
